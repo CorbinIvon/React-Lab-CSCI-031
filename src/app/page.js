e487e07c0043edd0a863637665a3acfaaa7cad73
Corbin Meier <corbinivon@gmail.com>
2023-11-13 17:55:25 -0800

    Supabase Integration

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,32 @@ import Title from '/src/app/components/PageTitle';
 import Footer from '/src/app/components/Footer';
 import PageContent from '/src/app/components/PageContent';
 import Card from '/src/app/components/Card';
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://anidbvxhgchtptfpozmt.supabase.co';
+const supabaseSecret = process.env.SUPABASE_SECRET;
+const supabase = createClient(supabaseUrl, supabaseSecret);
 
-export default function Home() {
+export default async function Home() {
+  const { data: Cards, error } = await supabase.from('Cards').select('*');
+  if (error) console.log(error);
+  console.log(supabaseSecret);
   return (
     <>
       <Navbar />
       <Title Title="Home" />
       <PageContent Content="Home page default content." />
       <div className="flex flex-wrap">
-        <Card
+        {Cards.map((card) => (
+          <Card
+            title={card.title}
+            content={card.content}
+            img={card.image_url}
+            url={card.source_url}
+            tags={card.tags}
+            timestamp={card.publish_time}
+          />
+        ))}
+        {/* <Card
           title="Employee Tracking System"
           content="A system designed to track when an employee is on site or not."
           img="https://i.pinimg.com/564x/fe/84/ed/fe84ed536a3f7290b237ec8dfb0a4511.jpg"
@@ -34,7 +51,7 @@ export default function Home() {
           url="/"
           tags={['Networking', 'File Management', 'Local Networking', 'Custom VPN']}
           timestamp="2022-10-01"
-        />
+        /> */}
       </div>
       <Footer />
     </>
