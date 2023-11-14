d6722a9784a46f824bad89a19e76d7bb24bf0ed2
Corbin Meier <corbinivon@gmail.com>
2023-11-14 12:23:15 -0800

    Adds better color theme to navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,7 +2,7 @@ import { CodeBracketIcon } from '@heroicons/react/24/solid';
 
 export default function Navbar() {
   const anchorStyle =
-    'px-2 py-2 hover:text-white rounded-lg mx-2 text-sm font-bold text-gray-800 shadow-md hover:shadow-lg m-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 from-blue-400 to-blue-600 bg-gradient-to-br hover:from-pink-500 hover:to-yellow-500';
+    'px-2 py-2 hover:text-white rounded-lg mx-2 text-sm font-bold text-gray-800 shadow-md hover:shadow-lg m-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 from-purple-400 to-blue-600 bg-gradient-to-br hover:from-blue-500 hover:to-sky-500';
   // Navbar should use a map operation to produce each anchor tag.
   const navBarContent = [
     { href: '/', text: 'HOME' },
