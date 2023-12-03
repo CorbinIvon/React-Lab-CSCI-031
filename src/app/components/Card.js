9f5bb584aa43c74cb05090fc38afd5d00e0c8b03
Corbin Meier <corbinivon@gmail.com>
2023-12-02 18:21:21 -0800

    Fixes card components on Blog. Now renders more details on click.

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,3 +1,6 @@
+'use client';
+import React, { useState } from 'react';
+
 export default function Card({
   key = -1,
   title = 'Placeholder Title',
@@ -7,6 +10,10 @@ export default function Card({
   timestamp = '2021-07-20T19:20:30.45+01:00',
   tags = ['placeholder', 'tag'],
 }) {
+  const [isContentVisible, setContentVisible] = useState(false);
+  const toggleContentVisibility = () => {
+    setContentVisible(!isContentVisible);
+  };
   const hoverEffect =
     'px-2 py-2 rounded-lg mx-2 shadow-md hover:shadow-lg m-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 max-w-sm max-h-sm cursor-pointer';
   const publishTime = timestamp ? (
@@ -18,7 +25,7 @@ export default function Card({
   const cardKey = key ? 'CardKey' + key : 'CardKey';
   return (
     <>
-      <div className={'CardComponent ' + hoverEffect} id={cardKey}>
+      <div className={'CardComponent ' + hoverEffect} id={cardKey} onClick={toggleContentVisibility}>
         <div className="bg-white shadow-md rounded-lg px-10 py-6 text-center">
           <img src={coverImageUrl} alt={coverImageUrl} height={150} width={150} className="w-full" />
           <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
@@ -29,28 +36,34 @@ export default function Card({
           </div>
         </div>
       </div>
-      <div className="absolute inset-0 bg-slate-900 bg-opacity-90 hidden" id={cardKey}>
-        <div className="absolute inset-0 lg:m-12 sm:m-12 p-4 bg-slate-700 rounded-2xl" id={cardKey}>
-          <div className="relative inset-0 flex flex-col lg:flex-row sm:flex-row rounded-2xl">
-            <div className="flex flex-col w-full lg:w-1/2 sm:w-1/2">
-              <div className="flex flex-col justify-center items-center lg:items-start sm:items-start">
-                <h1 className="text-5xl font-bold text-white mb-8">{title}</h1>
-                <p className="text-xl font-semibold text-white mb-8">{contentShort}</p>
-                <div className="flex flex-row justify-center lg:justify-start sm:justify-start">
-                  <div className="flex flex-row items-center">
-                    <div className="flex flex-col justify-center items-center">
-                      <p className="text-sm font-semibold text-white">{contentLong}</p>
+      {isContentVisible && (
+        <div
+          className="absolute inset-0 bg-slate-900 bg-opacity-90 z-10"
+          id={cardKey}
+          onClick={toggleContentVisibility}
+        >
+          <div className="absolute inset-0 lg:m-12 sm:m-12 p-4 bg-slate-700 rounded-2xl" id={cardKey}>
+            <div className="relative inset-0 flex flex-col lg:flex-row sm:flex-row rounded-2xl">
+              <div className="flex flex-col w-full lg:w-1/2 sm:w-1/2">
+                <div className="flex flex-col justify-center items-center lg:items-start sm:items-start">
+                  <h1 className="text-5xl font-bold text-white mb-8">{title}</h1>
+                  <p className="text-xl font-semibold text-white mb-8">{contentShort}</p>
+                  <div className="flex flex-row justify-center lg:justify-start sm:justify-start">
+                    <div className="flex flex-row items-center">
+                      <div className="flex flex-col justify-center items-center">
+                        <p className="text-sm font-semibold text-white">{contentLong}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col w-full lg:w-1/2 sm:w-1/2">
-              <img src={coverImageUrl} alt={coverImageUrl} className="w-full h-full object-cover rounded-2xl" />
+              <div className="flex flex-col w-full lg:w-1/2 sm:w-1/2">
+                <img src={coverImageUrl} alt={coverImageUrl} className="w-full h-full object-cover rounded-2xl" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
