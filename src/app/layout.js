474a04415705df8eeb962d9d42ca792b8b20e4a0
Corbin Meier <corbinivon@gmail.com>
2023-12-03 18:30:53 -0800

    Fixes BG bug with blog. Fixes#1

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,15 +11,16 @@ export const metadata = {
   title: "Corbin's Next.js Site",
   description: "Site for Corbin's Next.js project",
 };
-const colorGradients = ['from-gray-900', 'from-teal-900', 'from-purple-900', 'from-yellow-900'];
+// const colorGradients = ['from-gray-900', 'from-teal-900', 'from-purple-900', 'from-yellow-900'];
+const colorGradients = ['from-slate-900'];
 const randomIndex = Math.floor(Math.random() * colorGradients.length);
 const gradientFrom = colorGradients[randomIndex];
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* It was being weird and back ticks weren't working... */}
-      {/* <body className={inter.className + ' bg-gradient-to-b ' + gradientFrom + ' to-black'}> */}
-      <body>
+      <body className={inter.className + ' bg-gradient-to-b ' + gradientFrom + ' to-black'}>
+        {/* <body> */}
         <Theme appearance="dark" accentColor="purple" grayColor="slate" scaling="95%">
           {/* <ThemePanel /> */}
           <div className="flex flex-col min-h-screen">
